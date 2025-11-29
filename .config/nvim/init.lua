99573ddbc27f93aa971eeed171f83c3f07135e2a
Nils Israel <nils@sxda.io>
2025-11-29 01:43:07 +0100

    add nvim-tree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -30,3 +30,45 @@ require("lazy").setup("plugins", {
     notify = false,  -- Don't notify on config changes
   },
 })
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+  on_attach = function(bufnr)
+    local api = require('nvim-tree.api')
+
+    -- Default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- Yazi-like arrow key navigation
+    vim.keymap.set('n', '<Right>', api.node.open.edit, { buffer = bufnr, desc = 'Open' })
+    vim.keymap.set('n', '<Left>', api.node.navigate.parent_close, { buffer = bufnr, desc = 'Close Directory' })
+
+    -- Toggle hidden files with .
+    vim.keymap.set('n', '.', api.tree.toggle_hidden_filter, { buffer = bufnr, desc = 'Toggle hidden files' })
+  end,
+})
+
+-- Keybinding to toggle nvim-tree
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
